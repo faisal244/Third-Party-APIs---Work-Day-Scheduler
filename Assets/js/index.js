fef7b9bc2f0afc7d222f56178a3ba833943fda44
Faisal Mohammed <40631753+faisal244@users.noreply.github.com>
2022-07-24 18:02:50 +0100

    Created array of working hours time blocks
    - Updated bootstrap script to latest version

---
+++ Assets/js/index.js
@@ -0,0 +1,39 @@
+// Declaration of an array of hourly time blocks
+const workingHours = [
+	{
+		timeLabel: "09:00AM",
+		key: 9,
+	},
+	{
+		timeLabel: "10:00AM",
+		key: 10,
+	},
+	{
+		timeLabel: "11:00AM",
+		key: 11,
+	},
+	{
+		timeLabel: "12:00AM",
+		key: 12,
+	},
+	{
+		timeLabel: "13:00PM",
+		key: 13,
+	},
+	{
+		timeLabel: "14:00PM",
+		key: 14,
+	},
+	{
+		timeLabel: "15:00PM",
+		key: 15,
+	},
+	{
+		timeLabel: "16:00PM",
+		key: 16,
+	},
+	{
+		timeLabel: "17:00PM",
+		key: 17,
+	},
+];
